5be8d8c010efabc63c5a9ac339c3aa5a8373ab0c
profjdbayliss <jdbics@rit.edu>
2023-05-22 11:35:58 -0400

    Initial commit of project.

---
+++ ResilienceGame/Assets/Scripts/PlaceIcons.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaceIcons : MonoBehaviour
+{
+    public float2 Offset;
+    public float2 SizeOfBox;
+    public List<float2> HospitalLocations;
+    public List<float2> FireDeptLocations;
+    private static GameObject canvas;
+    private GameObject Hospital;
+    private GameObject FireTruck;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the canvas
+        canvas = GameObject.Find("Canvas");
+        Hospital = GameObject.Find("Hospital");
+        FireTruck = GameObject.Find("FireTruck");
+        
+        // take hospital image and place it on canvas at locations specified;
+        int locationCount = HospitalLocations.Count;
+        for (int i = 0; i < locationCount; i++)
+        {
+            // make a copy
+            GameObject hospital = Instantiate(Hospital);
+            // place
+            hospital.transform.position = new Vector3(HospitalLocations[i].x*SizeOfBox.x+Offset.x, -1*HospitalLocations[i].y*SizeOfBox.y+Offset.y, 0);
+            hospital.transform.SetParent (canvas.transform,false);
+        }
+        
+        locationCount = FireDeptLocations.Count;
+        for (int i = 0; i < locationCount; i++)
+        {
+            // make a copy
+            GameObject fire = Instantiate(FireTruck);
+            // place
+            fire.transform.position = new Vector3(FireDeptLocations[i].x*SizeOfBox.x+Offset.x, -1*FireDeptLocations[i].y*SizeOfBox.y+Offset.y, 0);
+            fire.transform.SetParent (canvas.transform,false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
