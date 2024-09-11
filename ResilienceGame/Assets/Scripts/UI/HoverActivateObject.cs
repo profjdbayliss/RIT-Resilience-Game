2fb93f626047cb49a7723e95b0e8fbbd60dee694
meaton96 <lammara96@gmail.com>
2024-09-11 12:49:02 -0400

    Added hover to facilities while moving the card if its bonus or action phase indicating you can play a card on the facility

--- ResilienceGame/Assets/Scripts/UI/HoverActivateObject.cs
+++ ResilienceGame/Assets/Scripts/UI/HoverActivateObject.cs
@@ -3,86 +3,134 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class HoverActivateObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class HoverActivateObject : MonoBehaviour
 {
-    public float delay = 0.5f; 
-    public float maxHeightOffset = 100;
-    public Boolean highlight = false;
-    public GameObject highlightObject;
-    public Boolean scale = false;
-    public GameObject scaleObject;
-    private Vector3 originalPosition;
-    private float timer = 0; 
-    private bool isHovering = false; 
-    private bool isScaled = false;
+    public float fadeTime = .25f;
+    public Image hoverImage;
 
-    void Start()
-    {
-        originalPosition = transform.position;
+    private bool isHovering = false;
+
+
+    private Coroutine fadeCoroutine;
+
+    public void StartFadeIn() {
+        hoverImage.color = new Color(hoverImage.color.r, hoverImage.color.g, hoverImage.color.b, 1f);
     }
-    void Update()
-    {
-        //will scale the object that is being hovered over and will count a timer until it shows extra card info. 
-        if (isHovering)
-        {
-            if (!isScaled & scale)
-            {
-                ScaleCard(.5f);
+    public void StartFadeOut() {
+        hoverImage.color = new Color(hoverImage.color.r, hoverImage.color.g, hoverImage.color.b, 0f);
 
-                timer += Time.deltaTime;
-                if (timer >= delay)
-                {
-                    // hoverObject.SetActive(true);
-                    if (scaleObject.transform.localPosition.y < originalPosition.y + maxHeightOffset)
-                    {
-                        scaleObject.transform.localPosition += new Vector3(0, 1, 0);
-                    }
-                }
-            }
-            if (highlight) highlightObject.SetActive(true);
-        }
-        //toggles the scaling effect to scale it back to its original size
-        else
-        { if (isScaled & scale) ScaleCard(-.5f); 
-          if (highlight) highlightObject.SetActive(false);
-        }
     }
+    //public void StartFadeIn() {
+    //    if (isHovering) return;
+    //    isHovering = true;
+    //    if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+    //    fadeCoroutine = StartCoroutine(FadeTo(1f));
+    //}
+    //public void StartFadeOut() {
+    //    if (!isHovering) return;
+    //    isHovering = false;
+    //    if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+    //    fadeCoroutine = StartCoroutine(FadeTo(0f));
+    //}
 
-    public void ScaleCard(float scaleAmount)
-    {
-        if(!isScaled)
-        this.gameObject.layer = 30; 
-        else
-        {
-            this.gameObject.layer = 6;
-        }
+    //private IEnumerator FadeTo(float targetAlpha) {
+    //    float startAlpha = hoverImage.color.a;
+    //    float elapsedTime = 0;
 
-        Vector2 tempScale = scaleObject.transform.parent.localScale;
-        Vector3 offset = scaleObject.transform.parent.localPosition;
-        tempScale.x = (float)(scaleObject.transform.parent.localScale.x + scaleAmount);
-        tempScale.y = (float)(scaleObject.transform.parent.localScale.y + scaleAmount);
-        offset.y = offset.y + scaleAmount * 200;
+    //    while (elapsedTime < fadeTime) {
+    //        elapsedTime += Time.deltaTime;
+    //        float currentAlpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeTime);
+    //        hoverImage.color = new Color(hoverImage.color.r, hoverImage.color.g, hoverImage.color.b, currentAlpha);
+    //        yield return null;
+    //    }
 
-        scaleObject.transform.parent.localScale = tempScale;
-        scaleObject.transform.parent.localPosition = offset;
-        
-        isScaled = !isScaled;
-    }
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        isHovering = true; 
-        timer = 0;     
-    }
+    //    hoverImage.color = new Color(hoverImage.color.r, hoverImage.color.g, hoverImage.color.b, targetAlpha);
+    //}
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        isHovering = false;       
-        timer = 0;
-        if (scale)
-        {
-            scaleObject.transform.SetLocalPositionAndRotation(new Vector3(), scaleObject.transform.rotation);
-            //scaleObject.SetActive(false);
-        }
-    }
+
+    #region old
+
+    //public float delay = 0.5f; 
+    //public float maxHeightOffset = 100;
+    //public Boolean highlight = false;
+    //public GameObject highlightObject;
+    //public Boolean scale = false;
+    //public GameObject scaleObject;
+    //private Vector3 originalPosition;
+    //private float timer = 0; 
+    //private bool isHovering = false; 
+    //private bool isScaled = false;
+
+    //void Start()
+    //{
+    //    originalPosition = transform.position;
+    //}
+    //void Update()
+    //{
+    //    //will scale the object that is being hovered over and will count a timer until it shows extra card info. 
+    //    if (isHovering)
+    //    {
+    //        if (!isScaled & scale)
+    //        {
+    //            ScaleCard(.5f);
+
+    //            timer += Time.deltaTime;
+    //            if (timer >= delay)
+    //            {
+    //                // hoverObject.SetActive(true);
+    //                if (scaleObject.transform.localPosition.y < originalPosition.y + maxHeightOffset)
+    //                {
+    //                    scaleObject.transform.localPosition += new Vector3(0, 1, 0);
+    //                }
+    //            }
+    //        }
+    //        if (highlight) highlightObject.SetActive(true);
+    //    }
+    //    //toggles the scaling effect to scale it back to its original size
+    //    else
+    //    { if (isScaled & scale) ScaleCard(-.5f); 
+    //      if (highlight) highlightObject.SetActive(false);
+    //    }
+    //}
+
+    //public void ScaleCard(float scaleAmount)
+    //{
+    //    if(!isScaled)
+    //    this.gameObject.layer = 30; 
+    //    else
+    //    {
+    //        this.gameObject.layer = 6;
+    //    }
+
+    //    Vector2 tempScale = scaleObject.transform.parent.localScale;
+    //    Vector3 offset = scaleObject.transform.parent.localPosition;
+    //    tempScale.x = (float)(scaleObject.transform.parent.localScale.x + scaleAmount);
+    //    tempScale.y = (float)(scaleObject.transform.parent.localScale.y + scaleAmount);
+    //    offset.y = offset.y + scaleAmount * 200;
+
+    //    scaleObject.transform.parent.localScale = tempScale;
+    //    scaleObject.transform.parent.localPosition = offset;
+
+    //    isScaled = !isScaled;
+    //}
+    //public void OnPointerEnter(PointerEventData eventData)
+    //{
+    //    isHovering = true; 
+    //    timer = 0;     
+    //}
+
+    //public void OnPointerExit(PointerEventData eventData)
+    //{
+    //    isHovering = false;       
+    //    timer = 0;
+    //    if (scale)
+    //    {
+    //        scaleObject.transform.SetLocalPositionAndRotation(new Vector3(), scaleObject.transform.rotation);
+    //        //scaleObject.SetActive(false);
+    //    }
+    //}
+
+    #endregion
 }
