333f35d0693a699b4bc254f245d4744d61020093
meaton96 <lammara96@gmail.com>
2024-10-22 17:27:10 -0400

    setting up lobby when starting the game

---
+++ ResilienceGame/Assets/Scripts/UI/LobbyItem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyItem : MonoBehaviour
+{
+    [SerializeField] private Color redColor;
+    [SerializeField] private Color blueColor;
+    [Header("UI Elements")]
+    public TextMeshProUGUI PlayerName;
+    public Image backgroundImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetPlayerNameAndTeam(string name, PlayerTeam team) {
+        PlayerName.text = name;
+        PlayerName.enabled = true;
+        if (team == PlayerTeam.Red) {
+            backgroundImage.color = redColor;
+        }
+        else {
+            backgroundImage.color = blueColor;
+        }
+    }
+}
---
+++ ResilienceGame/Assets/Scripts/UI/PlayerLobbyManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerLobbyManager : MonoBehaviour
+{
+    [SerializeField] private GameObject playerPopupPrefab;
+    [SerializeField] private RectTransform blueLeftParent;
+    [SerializeField] private RectTransform blueRightParent;
+    [SerializeField] private RectTransform redParent;
+
+    [SerializeField] private Color redColor;
+    [SerializeField] private Color blueColor;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
