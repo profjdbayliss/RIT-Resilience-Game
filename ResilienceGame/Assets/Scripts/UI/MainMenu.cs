4b89346156acbf83528627a2495524f0afc2f95b
meaton96 <lammara96@gmail.com>
2024-11-14 12:37:16 -0500

    working on updating rules on main menu

--- ResilienceGame/Assets/Scripts/UI/MainMenu.cs
+++ ResilienceGame/Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject rulesMenu;
+    [SerializeField] private string rulesPath;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,4 +33,26 @@ public class MainMenu : MonoBehaviour
     public void StartGame() {
         SceneManager.LoadScene("Network Login Scene");
     }
+    public void OpenPDF() {
+        string filePath = Path.Combine(Application.streamingAssetsPath, rulesPath + ".pdf");
+
+        if (File.Exists(filePath)) {
+#if UNITY_EDITOR
+            Process.Start(filePath);
+#elif UNITY_STANDALONE_WIN
+            Process.Start(filePath);
+#elif UNITY_STANDALONE_OSX
+            Process.Start("open", filePath);
+#elif UNITY_STANDALONE_LINUX
+            Process.Start("xdg-open", filePath);
+#else
+            Debug.LogError("Opening files not supported on this platform.");
+#endif
+        }
+        else {
+            UnityEngine.Debug.LogError("File not found: " + filePath);
+        }
+    }
+
+
 }
