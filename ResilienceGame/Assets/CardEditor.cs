edf8d71a3503d831b1f98a44e9ef5b86c89993dc
meaton96 <lammara96@gmail.com>
2024-12-03 13:06:05 -0500

    fix card prefab for editor

--- ResilienceGame/Assets/CardEditor.cs
+++ ResilienceGame/Assets/CardEditor.cs
@@ -1,9 +1,16 @@
+using SFB;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
 using UnityEngine;
 
 public class CardEditor : MonoBehaviour
 {
+
+    [SerializeField] private TextMeshProUGUI cardList;
+    [SerializeField] private TextMeshProUGUI deckTitle;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +18,47 @@ public class CardEditor : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
+
+    public void OpenDeck() {
+        // Check if the application supports file dialogs
+        string s = "";
+        if (Application.isEditor || Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.OSXPlayer) {
+            // Show the file browser dialog
+            string[] paths = StandaloneFileBrowser.OpenFilePanel("Open CSV File", "", "csv", false);
+
+            if (paths.Length > 0 && !string.IsNullOrEmpty(paths[0])) {
+                string filePath = paths[0];
+                try {
+                    // Read the CSV file contents
+                    string[] lines = File.ReadAllLines(filePath);
+
+                    // Process each line (for example, split by commas)
+                    foreach (string line in lines) {
+                        string[] values = line.Split(',');
+
+                        // Example: Output the CSV values to the console
+                        Debug.Log($"Line Data: {string.Join(", ", values)}");
+                        s += string.Join(", ", values) + "\n";
+
+
+                    }
+                    deckTitle.text = filePath;
+                    cardList.text = s;
+                }
+                catch (Exception e) {
+                    Debug.LogError($"Error reading CSV file: {e.Message}");
+                }
+            }
+            else {
+                Debug.Log("No file selected.");
+            }
+        }
+        else {
+            Debug.LogError("File browser dialogs are not supported on this platform.");
+        }
+    }
+
 }
---
+++ ResilienceGame/Assets/Scripts/Card Editor New/EditorCard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorCard : MonoBehaviour
+{
+    public string data;
+    public Card card;
+    public GameObject cardGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
