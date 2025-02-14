78a19fdc3f1ecb552529eca5073c6b17a89a519b
Emmett1203 <eem1203@g.rit.edu>
2025-02-14 11:13:27 -0500

    Added in a basic host explanation button.

--- ResilienceGame/Assets/Scripts/UI/ConnectHelp.cs
+++ ResilienceGame/Assets/Scripts/UI/ConnectHelp.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class ConnectHelp : MonoBehaviour
 {
     [SerializeField] private GameObject connectHelpCanvas;
+    [SerializeField] private GameObject connectHostCanvas;
     private bool isConnectHelpCanvasActive;
+    private bool isHostHelpCanvasActive;
     // Start is called before the first frame update
     void Start()
     {
         isConnectHelpCanvasActive = false;
+        isHostHelpCanvasActive = false;
         connectHelpCanvas.SetActive(isConnectHelpCanvasActive);
+        connectHostCanvas.SetActive(isHostHelpCanvasActive);
     }
 
     public void ToggleConnectHelpCanvas()
     {
         isConnectHelpCanvasActive = !isConnectHelpCanvasActive;
         connectHelpCanvas.SetActive(isConnectHelpCanvasActive);
+    }
+
+    public void ToggleHostHelpCanvas()
+    {
+        isHostHelpCanvasActive = !isHostHelpCanvasActive;
+        connectHostCanvas.SetActive(isHostHelpCanvasActive);
     }
 }
