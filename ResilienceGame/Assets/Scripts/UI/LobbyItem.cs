80af985477aa763c322a277e9ab750e9cfd22eda
Emmett1203 <eem1203@g.rit.edu>
2025-02-19 10:32:12 -0500

    revamped lobby to tell users to refresh when a user doesn't show their name.

--- ResilienceGame/Assets/Scripts/UI/LobbyItem.cs
+++ ResilienceGame/Assets/Scripts/UI/LobbyItem.cs
@@ -11,6 +11,7 @@ public class LobbyItem : MonoBehaviour
     [SerializeField] private Color missingPlayerColor;
     [Header("UI Elements")]
     public TextMeshProUGUI PlayerName;
+    [SerializeField] private GameObject PleaseRefresh;
     public Image backgroundImage;
 
     // Start is called before the first frame update
@@ -29,6 +30,7 @@ public class LobbyItem : MonoBehaviour
         PlayerName.enabled = true;
         Debug.Log($"is red? {team == PlayerTeam.Red}");
         Debug.Log($"is blue? {team == PlayerTeam.Blue}");
+        PleaseRefresh.SetActive(false);
         if (team == PlayerTeam.Red) {
             backgroundImage.color = redColor;
         }
@@ -42,6 +44,7 @@ public class LobbyItem : MonoBehaviour
     }
 
     public void SetMissingPlayer() {
+        PleaseRefresh.SetActive(true);
         PlayerName.enabled = false;
         backgroundImage.color = missingPlayerColor;
     }
