8f31051cb6a856209131a65dea90b3535c455e8c
Jye Crocker <jyefett22@gmail.com>
2025-04-15 19:51:30 -0400

    Tutorial UI buttons added with placeholder youtube links.

---
+++ ResilienceGame/Assets/Scripts/OpenYoutube.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class OpenYoutube : MonoBehaviour
+{
+    public void OpenBlueVideo()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+
+    public void OpenRedVideo()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=eeAZ5s2ZaGw");
+    }
+
+    public void OpenConnectionVideo()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=N6hVmn9FM7o");
+    }
+}
